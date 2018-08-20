5a5d34eca7eea8c85316cc79d1036cda2c8ae784
Ryland Herrick <ryalnd@gmail.com>
2018-08-20 16:55:16 -0500

    Define our Position class
    This combines all our options (size, grid, alignments, basis) into a
    shape on the grid.

--- src/position.ts
+++ src/position.ts
@@ -1,18 +1,44 @@
 import { Position, XAlignment, YAlignment, XGrid, YGrid } from './types';
+import Grid from './grid';
 
 export default class implements Position {
   readonly top: number;
   readonly left: number;
 
   constructor(
-    readonly height: number = 0,
-    readonly width: number = 0,
-    readonly xAlign: XAlignment = XAlignment.END,
-    readonly yAlign: YAlignment = YAlignment.BOTTOM,
+    readonly height: number,
+    readonly width: number,
+    readonly grid: Grid,
     readonly xGrid: XGrid = XGrid.INNER_LEFT,
     readonly yGrid: YGrid = YGrid.INNER_TOP,
+    readonly xAlign: XAlignment = XAlignment.END,
+    readonly yAlign: YAlignment = YAlignment.BOTTOM,
   ) {
-    this.top = 1;
-    this.left = 2;
+    this.top = this.calculateTop();
+    this.left = this.calculateLeft();
+  }
+
+  private calculateTop(): number {
+    const startingTop = this.grid.yScale(this.yGrid);
+
+    if (this.yAlign === YAlignment.TOP) {
+      return startingTop;
+    } else if (this.yAlign === YAlignment.BOTTOM) {
+      return startingTop - this.height;
+    } else {
+      return startingTop - this.height / 2;
+    }
+  }
+
+  private calculateLeft(): number {
+    const startingLeft = this.grid.xScale(this.xGrid);
+
+    if (this.xAlign === XAlignment.START) {
+      return startingLeft;
+    } else if (this.xAlign === XAlignment.END) {
+      return startingLeft - this.width;
+    } else {
+      return startingLeft - this.width / 2;
+    }
   }
 }
--- test/position.test.ts
+++ test/position.test.ts
@@ -1,22 +1,69 @@
-import { Position } from '../src';
+import { Grid, Position } from '../src';
+import { XAlignment, YAlignment, XGrid, YGrid } from '../src/types';
+
+const defaultRect = { height: 100, width: 100, top: 0, left: 0 };
 
 describe('Position', () => {
   let position;
+  let positionBuilder;
+  let grid;
+
+  beforeEach(() => {
+    grid = new Grid(defaultRect, defaultRect);
+  });
 
   it('returns the initialized height/width', () => {
-    position = new Position(1, 2);
+    position = new Position(1, 2, grid);
 
     expect(position.height).toEqual(1);
     expect(position.width).toEqual(2);
   });
 
-  describe('alignments', () => {
-    it('modifies the left value depending on the xAlign value');
-    it('modifies the top value depending on the yAlign value');
+  describe('grid basis', () => {
+    beforeEach(() => {
+      positionBuilder = (...args) => new Position(1, 2, grid, ...args);
+    });
+
+    it('modifies the left value depending on the xGrid value', () => {
+      position = positionBuilder(XGrid.INNER_LEFT);
+      const alignedPosition = positionBuilder(XGrid.OUTER_RIGHT);
+
+      expect(position.left).toEqual(-2);
+      expect(alignedPosition.left).toEqual(98);
+    });
+
+    it('modifies the top value depending on the yGrid value', () => {
+      position = positionBuilder(XGrid.INNER_LEFT, YGrid.INNER_TOP);
+      const alignedPosition = positionBuilder(
+        XGrid.INNER_LEFT,
+        YGrid.INNER_BOTTOM,
+      );
+
+      expect(position.top).toEqual(-1);
+      expect(alignedPosition.top).toEqual(99);
+    });
   });
 
-  describe('refs', () => {
-    it('modifies the left value depending on the xGrid value');
-    it('modifies the top value depending on the yGrid value');
+  describe('alignments', () => {
+    beforeEach(() => {
+      positionBuilder = (...args) =>
+        new Position(1, 2, grid, XGrid.INNER_LEFT, YGrid.INNER_TOP, ...args);
+    });
+
+    it('modifies the left value depending on the xAlign value', () => {
+      position = positionBuilder(XAlignment.END);
+      const alignedPosition = positionBuilder(XAlignment.START);
+
+      expect(position.left).toEqual(-2);
+      expect(alignedPosition.left).toEqual(0);
+    });
+
+    it('modifies the top value depending on the yAlign value', () => {
+      position = positionBuilder(XAlignment.END, YAlignment.BOTTOM);
+      const alignedPosition = positionBuilder(XAlignment.END, YAlignment.TOP);
+
+      expect(position.top).toEqual(-1);
+      expect(alignedPosition.top).toEqual(0);
+    });
   });
 });
